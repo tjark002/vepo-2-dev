88d0a7e24b46b168c7fd5fac9a2037b5b6099ce7
tjark002 <49152482+tjark002@users.noreply.github.com>
2026-03-13 08:37:41 +0100

    initial deploy

--- prisma/migrations/20260211120000_add_template_suffix/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `ProductConfigurationOptions` ADD COLUMN `templateSuffix` VARCHAR(191) NULL;
--- prisma/migrations/20260303074900_add_rounding_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `ProductConfigurationOptions` ADD COLUMN `roundingEnabled` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `roundingPrecision` VARCHAR(191) NOT NULL DEFAULT '1';
